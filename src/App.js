61e9094fdf88d05c0f874c36a50c718e7aa201c5
Shirley Li <liyingfe@msu.edu>
2025-11-06 22:54:36 -0500

    kk

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          was changed on feature
+          Learn React
         </a>
       </header>
     </div>
